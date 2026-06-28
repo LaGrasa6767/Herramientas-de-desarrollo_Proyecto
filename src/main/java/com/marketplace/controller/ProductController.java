a57d23ed24be49e04d77a45729414fd27cb7f535
Willian Siesquen <wsiesquenf@gmail.com>
2026-06-28 14:59:31 -0500

    Esta es la parte del SpringBoot del proyecto

--- src/main/java/com/marketplace/controller/ProductController.java
+++ src/main/java/com/marketplace/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
